f26d034a00a8c2756a8aaba28e48c1dcb181682c
Sayantan Chakraborty <sayantan.chakraborty@signify.com>
2021-04-24 16:25:03 +0530

    Initial Commit

---
+++ C19Info/C19InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  C19InfoApp.swift
+//  C19Info
+//
+//  Created by Sayantan Chakraborty on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct C19InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C19Info/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C19Info
+//
+//  Created by Sayantan Chakraborty on 24/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
